14c2733cb555cdfb528701f9200627e7402eff36
Robert Howden <robert.howden@current-rms.com>
2020-04-20 15:23:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,10 +6,22 @@ function subtract(a, b) {
   return a - b
 }
 
-function multiple(a, b) {
+function multiply(a, b) {
   return a * b
 }
 
 function divide(a, b) {
   return a / b
+}
+
+function increment(a) {
+  return ++a
+}
+
+function decrement(a) {
+  return --a
+}
+
+function makeInt(a) {
+  return parseInt(a)
 }
